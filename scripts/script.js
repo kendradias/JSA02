0cd7900ff10e0eac23dde056c770245c0994a316
Kendra Dias <kendra.dias@hotmail.com>
2024-10-31 13:17:34 -0700

    ensured every button id was unique

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-console.log('hey there')
+'use strict';
+
+// game object
+const game = {
+    isRunning: false,
+    wasRunning: false,
+    currentScreen: 'splash-screen',
+
+    //switch screen function
+    switchScreen: function(screen) {
+        this.currentScreen = screen;
+        $('.screen').hide();
+        $(`#${screen}`).show();
+
+        //handling button visibility in header
+        if (screen === 'game-screen') {
+            $('#header-quit-btn').show();
+        } else {
+            $('#header=quit-btn').hide();
+            this.isRunning = false;
+        }
+
+    },
+
+    //show help function
+    showHelp: function() {
+
+    },
+
+    //toggleRunning function
+    toggleRunning: function () {
+        this.isRunning = !this.isRunning;
+        $('#play-pause-btn').text(this.isRunning ? 'Pause' : 'Play');
+    }
+};
