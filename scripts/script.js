030f28bd5e49c77476050b30a76e3f79d3effc96
Kendra Dias <kendra.dias@hotmail.com>
2024-11-02 13:20:25 -0700

    Added more descriptive comments for js

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,21 @@ const game = {
 
     //switch screen function
     switchScreen: function(screen) {
+        //update current screen property
         this.currentScreen = screen;
-        console.log(`Current screen is ${screen}`);
+        //hide all screens and show selected screen
         $('.screen').hide();
         $(`#${screen}`).show();
 
-        //handling button visibility in header
+        //handling button visibility in header based on selected screen
+        //show quit button in game screen, hide on other screens
         if (screen === 'game-screen') {
             $('#header-quit-btn').show();
         } else {
             $('#header-quit-btn').hide();
             this.isRunning = false;
         } 
-
+        //toggle help button visibility, hide if game over screen
         if (screen === "game-over-screen") {
             $('#help-btn').hide();
         } else {
@@ -31,52 +33,67 @@ const game = {
     //show help function
     showHelp: function() {
         if (this.currentScreen === 'game-screen') {
+            //if game screen, save running state and pause
             this.wasRunning = this.isRunning;
             this.isRunning = false;
             $('#gameplay-modal').modal('show');
         } else {
+            //show setup instructions modal on splash screen
             $('#setup-modal').modal('show');
         }
 
     },
 
-    //toggle game function
+    //toggle game function 
     toggleRunning: function () {
         this.isRunning = !this.isRunning;
+        //update button text to display play/pause 
         $('#play-pause-btn').text(this.isRunning ? 'Pause' : 'Play');
     }
 };
 
-//event Listeners
+//Event Listeners
 $(document).ready(function() {
-    //header buttons
+    //HEADER BUTTONS
+    //display help modal on click
     $('#help-btn').click(() => game.showHelp());
+    //quit game and return to splash screen on click
     $('#quit-btn').click(() => game.switchScreen('splash-screen'));
 
-    //splash screen buttons
+    //SPLASH SCREEN BUTTONS
+    //start game and navigate to game screen on click 
     $('#play-game-btn').click(() => game.switchScreen('game-screen'));
 
-    //game screen buttons
+    //GAME SCREEN BUTTONS
+    //toggle game state on click
     $('#play-pause-btn').click(() => game.toggleRunning());
+    //end game and navigate to game over screen on click
     $('#end-game-btn').click(() => game.switchScreen('game-over-screen'));
+    //redirect back to splash screen on click
     $('#final-quit-btn').click(() => game.switchScreen('splash-screen'));
 
-    //game over screen buttons
+    //GAME OVER BUTTONS
+    // switch to game screen on click
     $('#play-again-btn').click(() => game.switchScreen('game-screen'));
+    //redirect back to splash screen on click
     $('#final-quit-btn').click(() => game.switchScreen('splash-screen'));
 
-    //modal handlers
+    //Modal Handlers
+
+    //switch from setup to gameplay instructions when more info is clicked
     $('#more-info-btn').click(() => {
         $('#setup-modal').modal('hide');
         $('#gameplay-modal').modal('show');
     });
 
+    // restore the game running state after closing gameplay modal
     $('#gameplay-modal').on('hidden.bs.modal', () => {
         if (game.currentScreen === 'game-screen') {
             game.isRunning = game.wasRunning;
         }
     });
 
+    // restore the game running state after closing setup modal
     $('#setup-modal').on('hidden.bs.modal', () => {
         if (game.currentScreen === 'game-screen') {
             game.isRunning = game.wasRunning;
